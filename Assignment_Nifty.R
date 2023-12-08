af4700547d50d5b4dbc19660d4ab7f2eb07f8793
David Ogbeide <135287366+DavidOgbeide@users.noreply.github.com>
2023-12-08 05:34:06 -0500

    Update Assignment_Nifty.R

--- Assignment_Nifty.R
+++ Assignment_Nifty.R
@@ -11,6 +11,9 @@ library(rstatix)
 library(caret)
 library(corrplot)
 
+# Question: "Can the variations in the 'Nifty' index be reasonably explained by changes in the values of the 'DJI' (Dow Jones Industrial Average), 'Vix' (Volatility Index),
+# 'SP500' (S&P 500), 'Brent_Crude' (Brent Crude Oil Prices), 'USD_INR' (USD to INR Exchange Rate), and 'CBOE_10Y' (CBOE Interest Rate 10-Year T-Note) over the observed 
+# time period?"
 
 nifty.df <- read.csv("C:/Users/drdav/Downloads/Nifty.csv")
 head(nifty.df)
